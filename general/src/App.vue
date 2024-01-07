1899d1b3c9c5e5baa7ae0910f01610db78caedcb
rainicoying <rainicong@magicubedu.com>
2024-01-07 13:42:14 +0800

    Deploy v20240107 - built 5

---
+++ general/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import LuckyDraw from './components/LuckyDraw.vue';
+</script>
+
+<template>
+	<v-container class="contentContainer">
+		<LuckyDraw></LuckyDraw>
+	</v-container>
+</template>
+
+<style scoped>
+.contentContainer {
+	width: 900px;
+	height: 100%;
+}
+
+@media screen and (max-width: 960px) {
+	.contentContainer {
+		max-width: 800px;
+	}
+}
+
+@media screen and (max-width: 600px) {
+	.contentContainer {
+		max-width: 560px;
+	}
+}
+</style>
